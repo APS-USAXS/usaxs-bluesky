87ec787bb2cad97eb2a00ebb7abf99e7f7f5ba18
Pete Jemian <prjemian@gmail.com>
2022-01-21 17:06:34 -0600

    MNT #494 update user's heater plan

--- user/heater_profile.py
+++ user/heater_profile.py
@@ -1,10 +1,13 @@
 """
 Run a temperature profile on the sample heater as a Bluesky plan.
 
-This file defines a function named ``planHeaterProcess()``
+This file defines a function (bluesky "plan") named
+    ``planHeaterProcess()``
 that runs the desired temperature profile schedule.
 All configuration is communicated via EPICS PVs
 which are interfaced here as ophyd EpicsSignal objects.
+Other plans and functions are used to simplify the
+interface in planHeaterProcess().
 
 Called (via ``import``) from ``heater_profile_process.py``
 (which is started/stopped/restarted from ``heater_profile_manager.sh``),
@@ -13,93 +16,89 @@ both of which are in directory ``~/.ipython/profile/bluesky/usaxs_support/``.
 See https://github.com/APS-USAXS/ipython-usaxs/issues/482 for details.
 """
 
+from apstools import devices
 from bluesky import plan_stubs as bps
-from ophyd import Component
-from ophyd import Device
 from ophyd import EpicsSignal
 from ophyd import EpicsSignalRO
-from ophyd import EpicsSignalWithRBV
-from ophyd import PVPositioner
-from ophyd import Signal
+
 import datetime
 import os
 import time
 
 
 SECOND = 1
-MINUTE = 60*SECOND
-HOUR = 60*MINUTE
+MINUTE = 60 * SECOND
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
+WEEK = 7 * DAY
+
+# Create devices here so we remain independent of the instrument package.
+exit_request_signal = EpicsSignal("9idcLAX:bit14", name="exit_request_signal")
+linkam_ci94 = devices.Linkam_CI94_Device("9idcLAX:ci94:", name="linkam_ci94")
+linkam_tc1 = devices.Linkam_T96_Device("9idcLINKAM:tc1:", name="linkam_tc1")
 
 # write output to log file in userDir, name=MMDD-HHmm-heater-log.txt
 user_dir = EpicsSignalRO("9idcLAX:userDir", name="user_dir", string=True)
-user_dir.wait_for_connection()
+
+for o in (exit_request_signal, linkam_ci94, linkam_tc1, user_dir):
+    o.wait_for_connection()
+
 log_file_name = os.path.join(
     user_dir.get(),
     datetime.datetime.now().strftime("%m%d-%H%M-heater-log.txt")
 )
 
-# Can't call the instrument package in this module.
-# Thus, we must re-define these devices here
+# make a common term for the ramp rate (devices use different names)
+linkam_ci94.ramp = linkam_ci94.rate
+linkam_tc1.ramp = linkam_tc1.ramprate
+
+linkam = linkam_tc1     # choose which one
+# linkam = linkam_ci94     # choose which one
+
+# set tolerance for "in position" (Python term, not an EPICS PV)
+# note: done = |readback - setpoint| <= tolerance
+linkam.temperature.tolerance.put(1.0)
+# sync the "inposition" computation
+linkam.temperature.cb_readback()
 
 
-class FeatureMixin(Device):
-
-    @property
-    def settled(self):
-        return self.done.get() == self.done_value
-
-class Linkam_CI94_Device(FeatureMixin, PVPositioner):
-    """The old Linkam controller."""
-    readback = Component(EpicsSignalRO, "temp", kind="normal")
-    setpoint = Component(EpicsSignal, "setLimit", kind="omitted")
-    ramp = Component(EpicsSignal, "setRate", kind="config")
-    done = Component(Signal, value=1, kind="omitted")
-    done_value = True
-    tolerance = Component(Signal, value=1, kind="config")
-    actuate = Component(EpicsSignal, "start.PROC", kind="omitted")
-    actuate_value = 1
-    stop_signal = Component(EpicsSignal, "stop.PROC", kind="omitted")
-    stop_value = 1
-    status_message = Component(EpicsSignalRO, "status", kind="config")
-
-    def cb_done(self, *args, **kwargs):
-        """Is readback close enough to setpoint?"""
-        diff = self.readback.get() - self.setpoint.get()
-        decision = abs(diff) <= abs(self.tolerance.get())
-        self.done.put(decision)
-
-    def __init__(self, prefix="", *, egu="", **kwargs):
-        PVPositioner.__init__(self, prefix, egu=egu, **kwargs)
-        self.readback.subscribe(self.cb_done)
-        self.setpoint.subscribe(self.cb_done)
+class StopHeaterPlan(Exception):
+    "Exception to stop the heater plan is stopping."
 
 
-class Linkam_T96_Device(FeatureMixin, PVPositioner):
-    """The newer Linkam controller."""
-    readback = Component(EpicsSignalRO, "temperature_RBV", kind="normal")
-    setpoint = Component(EpicsSignalWithRBV, "rampLimit", kind="omitted")
-    ramp = Component(EpicsSignalWithRBV, "rampRate", kind="config")
-    done = Component(EpicsSignalRO, "rampAtLimit_RBV", kind="omitted")
-    actuate = Component(EpicsSignalWithRBV, "heating", kind="config", string=True)
-    actuate_value = "On"
-    stop_signal = Component(EpicsSignalWithRBV, "heating", kind="config", string=True)
-    stop_value = "Off"
+def readable_time(duration, rounding=2):
+    """
+    Return a string representation of the duration.
 
+    EXAMPLES::
+    
+        readable_time(425) --> '7m 5s'
+        readable_time(1425) --> '23m 45s'
+        readable_time(21425) --> '5h 57m 5s'
+        readable_time(360) --> '6m'
+        readable_time(62.123) --> '1m 2.12s'
+    """
+    weeks = int(duration / WEEK)
+    known = weeks * WEEK
 
-linkam_exit = EpicsSignal("9idcLAX:bit14", name="linkam_exit")
-linkam_ci94 = Linkam_CI94_Device("9idcLAX:ci94:", name="linkam_ci94", egu="C")
-linkam_tc1 = Linkam_T96_Device("9idcLINKAM:tc1:", name="linkam_tc1", egu="C")
+    days = int((duration - known) / DAY)
+    known += days * DAY
 
-for o in (linkam_ci94, linkam_exit, linkam_tc1):
-    o.wait_for_connection()
+    hours = int((duration - known) / HOUR)
+    known += hours * HOUR
 
-#linkam = linkam_tc1     # choose which one
-linkam = linkam_ci94     # choose which one
+    minutes = int((duration - known) / MINUTE)
+    known += minutes * MINUTE
 
+    seconds = round(duration - known, rounding)
+    db = dict(w=weeks, d=days, h=hours, m=minutes, s=seconds)
 
-def isotime():
-    """ISO-8601 format time, ms precision."""
-    return datetime.datetime.now().isoformat(sep=" ", timespec='milliseconds')
+    s = [
+        f"{v}{k}"
+        for k, v in db.items()
+        if v != 0
+    ]
+    return " ".join(s)
 
 
 def log_it(text):
@@ -112,102 +111,109 @@ def log_it(text):
             f.write(f"# from: {__file__}\n")
     with open(log_file_name, "a") as f:
         # write the payload
-        f.write(f"{isotime()}: {text}\n")
+        dt = datetime.datetime.now()
+        # ISO-8601 format time, ms precision
+        iso8601 = dt.isoformat(sep=" ", timespec='milliseconds')
+        f.write(f"{iso8601}: {text}\n")
 
 
-def report():
+def linkam_report():
     """Report current values for selected controller."""
-    c = linkam
+    units = linkam.units.get()
     log_it(
-        f"{c.name}"
-        f" T={c.readback.get():.1f}{c.egu}"
-        f" setpoint={c.setpoint.get():.1f}{c.egu}"
-        f" ramp:{c.ramp.get()}"
-        f" settled: {c.settled}"
-        f" done: {c.done.get()}"
+        f"{linkam.name}"
+        f" T={linkam.temperature.position:.1f}{units}"
+        f" setpoint={linkam.temperature.setpoint.get():.1f}{units}"
+        f" ramp:{linkam.ramp.get()}"
+        f" settled: {linkam.temperature.inposition}"
+        f" done: {linkam.temperature.done.get()}"
     )
+
+
+def change_ramp_rate(value):
+    """BS plan: change controller's ramp rate."""
+    yield from bps.mv(linkam.ramp, value)
+    log_it(
+        f"Change {linkam.name} rate to {linkam.ramp.get():.0f} C/min"
+    )
+
+
+def check_for_exit(t0):
+    """
+    BS plan: Tell linkam controller to stop (a temperature change in progress).
+
+    Can't call linkam.temperature.stop() since that has blocking code.
+    Implement that method here by holding current position.
+
+    Raise ``StopHeaterPlan`` exception is exit was requested.
+    Otherwise return ```None``
+    """
+    # Watch for user exit while waiting
+    if exit_request_signal.get() in (0, exit_request_signal.enum_strs[0]):
+        # no exit requested
+        return
+
+    yield from bps.mv(linkam.temperature, linkam.position)
+    minutes = (time.time() - t0) / 60
+    log_it(
+        "User requested exit during set"
+        f" after {minutes:.2f}m."
+        " Stopping the heater."  # FIXME: Stopping? or holding at temperature?
+    )
+    linkam_report()
+    raise StopHeaterPlan(f"Stop requested after {minutes:.2f}m")
+
+
+def linkam_change_setpoint_and_wait(value):
+    """
+    BS plan: change the temperature setpoint and wait for inposition.
+
+    To change temperature and wait:
+       bps.mv(linkam.temperature, value)
+       Turns on heater power (if it was off).
+    To just change temperature, do not wait (hint: use the setpoint directly):
+       bps.mv(linkam.temperature.setpoint, value)
+       Does NOT turn on heater power.
+    """
+    t0 = time.time()
+    yield from bps.mv(
+        linkam.temperature.setpoint, value,
+    )
+    log_it(f"Change {linkam.name} setpoint to {linkam.setpoint.get():.2f} C")
+    while not linkam.temperature.inposition:
+        yield from check_for_exit(t0)
+        yield from bps.sleep(1)
+    log_it(f"Done, that took {time.time()-t0:.2f}s")
+    linkam_report()
+
+
+def linkam_hold(duration):
+    """BS plan: hold at temperature for the duration (s)."""
+    log_it("{linkam.name} holding for {readable_time(duration)}")
+    t0 = time.time()
+    time_expires = t0 + duration
+    while time.time() < time_expires:
+        yield from check_for_exit(t0)
+        yield from bps.sleep(1)
+    log_it(f"{linkam.name} holding period ended")
+    linkam_report()
 
 
 def planHeaterProcess():
     """BS plan: Run one temperature profile on the sample heater."""
     log_it(f"Starting planHeaterProcess() for {linkam.name}")
-    report()
+    linkam_report()
 
-    yield from bps.mv(
-        linkam.ramp, 3,
-    )
-    log_it(
-        f"Change {linkam.name.get()} rate to {linkam.ramp.get():.0f} C/min"
-    )
-
-    t0 = time.time()
-    # note: bps.mv waits until OBJECT.done.get() == 1 (just like a motor)
-    # bps.abs_set only _sets_ the setpoint, must wait in a later step
-    yield from bps.abs_set(
-        linkam, 1083,
-    )
-    log_it(
-        f"Change {linkam.name.get()} setpoint to {linkam.setpoint.get():.2f} C"
-    )
-    while linkam.done.get() not in (1, "Done"):
-        if linkam_exit.get():  # Watch for user exit while waiting
-            yield from bps.abs_set(linkam.stop_signal, linkam_stop_value)
-            log_it(
-                "User requested exit during set"
-                f" after {(time.time()-t0)/60:.2f}m."
-                " Stopping the heater."
-            )
-            report()
-            return
-        yield from bps.sleep(1)
-    log_it(f"Done, that took {time.time()-t0:.2f}s")
-    report()
-
-    # two hours = 2 * HOUR, two minutes = 2 * MINUTE
-    log_it("{linkam.name.get()} holding for 3 hours")
-    t0 = time.time()
-    time_expires = t0 + 3 * HOUR
-    while time.time() < time_expires:
-        if linkam_exit.get():
-            yield from bps.abs_set(linkam.stop_signal, linkam_stop_value)
-            log_it(
-                "User requested exit during hold"
-                f" after {(time.time()-t0)/60:.2f}m."
-                " Stopping the heater."
-            )
-            report()
-            return
-        yield from bps.sleep(1)
-    log_it("{linkam.name.get()} holding period ended")
-    report()
-
-     # Change rate to 3 C/min :
-    yield from bps.mv(
-        linkam.ramp, 3,
-    )
-    log_it(f"Change {linkam.name.get()} rate to {linkam.ramp.get():.0f} C/min")
-    t0 = time.time()
-    yield from bps.abs_set(
-        linkam, 40,
-    )
-    log_it(f"Change {linkam.name.get()} setpoint to {linkam.setpoint.get():.2f} C")
-    while linkam.done.get() not in (1, "Done"):
-        if linkam_exit.get():  # Watch for user exit while waiting
-            yield from bps.abs_set(linkam.stop_signal, linkam_stop_value)
-            log_it(
-                "User requested exit during set"
-                f" after {(time.time()-t0)/60:.2f}m."
-                " Stopping the heater."
-            )
-            report()
-            return
-        yield from bps.sleep(1)
-    # note: bps.mv waits until OBJECT.done.get() == 1 (just like a motor)
-    log_it(f"Done, that took {time.time()-t0:.2f}s")
-    report()
+    try:
+        yield from change_ramp_rate(3)
+        yield from linkam_change_setpoint_and_wait(1083)
+        # two hours = 2 * HOUR, two minutes = 2 * MINUTE
+        yield from linkam_hold(3 * HOUR)
+        yield from change_ramp_rate(3)
+        yield from linkam_change_setpoint_and_wait(40)
+    except StopHeaterPlan:
+        return
 
     # DEMO: signal for an orderly exit after first run
-    yield from bps.mv(linkam_exit, True)
+    yield from bps.mv(exit_request_signal, True)
     # -----------------------------------
-
-
