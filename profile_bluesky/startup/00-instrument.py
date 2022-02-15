7935ff9433072d239e06b5c494d1a660f63146a2
Pete Jemian <prjemian@gmail.com>
2022-02-15 16:24:59 -0600

    DBG #546

--- profile_bluesky/startup/00-instrument.py
+++ profile_bluesky/startup/00-instrument.py
@@ -3,7 +3,12 @@ start bluesky in IPython session for 9-ID-C USAXS
 """
 
 
+import faulthandler
 import pathlib, sys
+
+# see https://docs.python.org/3/library/faulthandler.html
+# and https://github.com/APS-USAXS/ipython-usaxs/issues/546
+faulthandler.enable()
 
 path = pathlib.Path.home() / ".ipython" / "profile_bluesky" / "startup"
 if str(path) not in sys.path:
