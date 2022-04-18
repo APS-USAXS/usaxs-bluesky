cb8457afa72fcfb4f6850a5be8b85bfe40275565
Pete Jemian <prjemian@gmail.com>
2022-04-18 12:27:26 -0500

    REF #522

--- profile_bluesky/startup/instrument/devices/pilatus_module.py
+++ profile_bluesky/startup/instrument/devices/pilatus_module.py
@@ -1,6 +1,5 @@
-
 """
-Dectris Pilatus area detectors
+Dectris Pilatus area detectors.
 """
 
 __all__ = [
@@ -23,6 +22,8 @@ from .area_detector_common import DATABROKER_ROOT_PATH
 from .area_detector_common import EpicsDefinesHDF5FileNames
 from .area_detector_common import _validate_AD_FileWriter_path_
 
+import pathlib
+
 
 # path for HDF5 files (as seen by EPICS area detector HDF5 plugin)
 # path seen by detector IOC
@@ -33,8 +34,40 @@ READ_HDF5_FILE_PATH_PILATUS = "/share1/USAXS_data/test/pilatus/%Y/%m/%d/"
 _validate_AD_FileWriter_path_(WRITE_HDF5_FILE_PATH_PILATUS, DATABROKER_ROOT_PATH)
 
 
-class MyPilatusHDF5Plugin(HDF5Plugin, FileStoreHDF5IterativeWrite):
+class MyPilatusHDF5Plugin(EpicsDefinesHDF5FileNames):
     """adapt HDF5 plugin for Pilatus detector"""
+
+    # fixes one problem, MUST end with path delimiter
+    @property
+    def write_path_template(self):
+        rootp = self.reg_root
+        delimiter = "/"
+        if self.path_semantics == 'posix':
+            ret = pathlib.PurePosixPath(self._write_path_template)
+        elif self.path_semantics == 'windows':
+            ret = pathlib.PureWindowsPath(self._write_path_template)
+            delimiter = "\\"
+        elif self.path_semantics is None:
+            # We are forced to guess which path semantics to use.
+            # Guess that the AD driver is running on the same OS as this client.
+            ret = pathlib.PurePath(self._write_path_template)
+        else:
+            # This should never happen, but just for the sake of future-proofing...
+            raise ValueError(f"Cannot handle path_semantics={self.path_semantics}")
+
+        if self._read_path_template is None and rootp not in ret.parents:
+            if not ret.is_absolute():
+                ret = rootp / ret
+            else:
+                raise ValueError(
+                    ('root: {!r} in not consistent with '
+                     'read_path_template: {!r}').format(rootp, ret))
+
+        return f"{ret}{delimiter}"  # THIS is the fix, MUST end with delimiter
+
+    @write_path_template.setter
+    def write_path_template(self, val):
+        self._write_path_template = val
 
 
 class MyPilatusDetector(SingleTrigger, AreaDetector):
@@ -44,7 +77,7 @@ class MyPilatusDetector(SingleTrigger, AreaDetector):
     image = ADComponent(ImagePlugin, "image1:")
 
     hdf1 = ADComponent(
-        EpicsDefinesHDF5FileNames,
+        MyPilatusHDF5Plugin,
         suffix = "HDF1:",
         root = DATABROKER_ROOT_PATH,
         write_path_template = WRITE_HDF5_FILE_PATH_PILATUS,
